a40724369d10fc7008b7aca4fe2815cd4d1fc16c
Tymoniden <timo.steib@gmail.com>
2022-11-01 22:27:00 +0100

    rename client endpoint overlay

---
+++ BattleBuddy/BattleBuddy/Ui/ClientEndpointOverlay.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BattleBuddy.Ui
+{
+    /// <summary>
+    /// Interaction logic for ClientConnectionOverlay.xaml
+    /// </summary>
+    public partial class ClientEndpointOverlay : UserControl
+    {
+        public ClientEndpointOverlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
